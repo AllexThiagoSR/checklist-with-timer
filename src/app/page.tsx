a010e798bea5127b4a5331be797dc1d33746360f
AllexThiagoSR <allexthiagodev@gmail.com>
2025-07-13 13:39:22 -0300

    feat: Create the tabs on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+'use client'
+import { Tab, Tabs } from "@heroui/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello World!
-    </div>
+    <section className="h-full flex flex-col">
+      <header className="flex justify-center p-4">
+        <h1 className="text-6xl">Checklist With Timer</h1>
+      </header>
+      <main className="p-10 grow">
+        <Tabs defaultSelectedKey="checklist" isVertical>
+          <Tab className="w-full h-full" key="checklist" title="Checklist"></Tab>
+          <Tab key="add-items" title="Add">Add</Tab>
+        </Tabs>
+      </main>
+    </section>
   );
 }
